577ba59b2d6759bac7496645b64c703c0f82991d
Eugene <eugene@ZipcodersMBP15.fios-router.home>
2022-06-24 09:11:52 -0400

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int solution = (int)(Math.random()*100 + 1);
+            System.out.println("Guess the number!");
+            int number =0;
+            while (number !=solution){
+                number = scan.nextInt();
+                if (number > solution){
+                    System.out.println("Too High!");
+                } else if (number < solution){
+                    System.out.println("Too Low!");
+
+                } else {
+                    System.out.println("CONGRATULATIONS YOU GET NOTHING!");
+                    break;
+                }
+
+
+        }
     }
 }
